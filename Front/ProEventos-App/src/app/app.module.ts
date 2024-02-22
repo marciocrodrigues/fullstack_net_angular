2fcee951fb6a14aa261d6284e42cdec75dc9c999
Marcio Costa Rodrigues <marciodeath@hotmail.com>
2024-02-22 00:47:17 -0300

    Inclusao funcionalidade filtro grid e alteracao layout

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localePT from '@angular/common/locales/pt';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+registerLocaleData(localePT);
+
 
 @NgModule({
   declarations: [
@@ -22,10 +27,13 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule,
-    CollapseModule.forRoot()
+    BrowserAnimationsModule,    
+    FormsModule,
+    CollapseModule.forRoot(),
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-br' },
+   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -10,6 +10,10 @@ import { environment } from 'src/environments/environment';
 export class EventosComponent implements OnInit {
 
   public eventos: any = [];
+  public eventosFiltrados: any = [];
+
+  exibirImagem = false;
+  private _filtroLista: string = '';
 
   constructor(
     private readonly http: HttpClient
@@ -19,11 +23,35 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  public get filtroLista(): string {
+    return this._filtroLista;    
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  private filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; local: string }) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
+
   public getEventos(): void {
     this.http.get(`${environment.baseUrl}eventos`).subscribe(
-      response => this.eventos = response,
+      response =>{
+        this.eventos = response;
+        this.eventosFiltrados = response;
+      },
       error => console.log(error)
     );
+  }
+
+  toogleImage() {
+    this.exibirImagem = !this.exibirImagem;
   }
 
 }
