2b9466fba1970f39f6b26e9d5d34a3407763d61f
Marcio Costa Rodrigues <marciodeath@hotmail.com>
2024-03-07 23:12:23 -0300

    feat: Adiciona data annotation nos DTOs

--- Back/src/ProEventos/ProEventos.Application/Dtos/EventoDto.cs
+++ Back/src/ProEventos/ProEventos.Application/Dtos/EventoDto.cs
@@ -1,15 +1,33 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProEventos.Application.Dtos
 {
     public class EventoDto
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Local { get; set; }
+
+        [Display(Name = "Data do Evento")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DataType(DataType.Date, ErrorMessage = "{0} invalida")]
         public string DataEvento { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "{0} deve ter entre 3 a 100 caracteres")]
         public string Tema { get; set; }
+
+        [Display(Name = "Qtd Pessoas")]
+        [Range(1, 120000, ErrorMessage = "{0} não pode ser menor que 1 e maior que 120.000")]
         public int QtdPessoas { get; set; }
+
+        [RegularExpression(@".*\.(gif|jpe?g|bmp|png)$", ErrorMessage = "Não é uma imagem válida, (gif, jpg, jpeg, bmp ou png)")]
         public string ImageURL { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Phone(ErrorMessage = "O campo {0} está com número inválido")]
         public string Telefone { get; set; }
         public IEnumerable<LoteDto> Lotes { get; set; }
         public IEnumerable<RedeSocialDto> RedesSociais { get; set; }
